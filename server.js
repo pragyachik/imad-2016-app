8ebb9b06bbabbed5cec83f1adc24da737400283b
Pragya <pragyachik@gmail.com>
2016-10-29 09:29:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.get('/article-one',function(req, res){
     res.send('This is the house of cats');
 });
 
+app.get('/article-two',function(req, res){
+    res.send('This is the house of dogs');
+});
+
+app.get('/article-three',function(req, res){
+    res.send('This is the house of birds');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
