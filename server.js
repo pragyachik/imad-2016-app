2f7fd98a5955529b55e3848088cbf7b541f1f8a9
Pragya <pragyachik@gmail.com>
2016-10-29 14:10:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', createTemplate(articleOne)));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function(req, res){
