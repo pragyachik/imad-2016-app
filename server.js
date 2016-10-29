272f1e03dd9dfee2863ea53f925ca97be974a268
Pragya <pragyachik@gmail.com>
2016-10-29 14:06:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,56 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne = {
+    title:"House of Cats | Pragya",
+    heading:"Cat Parade",
+    date:"October 29th, 2016.",
+    content: 
+    `<p>The cat parade starts at 5 am tomorrow. People of all nationalities are invited. All cat owners are encouraged to bring their cat along with them. The purpose of this parade is to find the healthiest, prettiest and smartest cat through interesting social experiments while giving the cats and their owners a great time. Food and shelter will be provided to upto 1000 kitty cats. Donations for future events of this sort are encouraged. This web page is dedicated to future discussions on this topic amongst all the participating cat-lovers. Have a good day!
+    </p>`
+};
+
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var htmlTemplate=
+    `<html>
+        <head>
+            <title>$(title)</title>
+            <meta name="viewport" content="width-device-width, initial-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href='/'>Home</a>
+                </div>   
+                <hr>
+                <h3>
+                    $(heading)
+                </h3>
+                <div>
+                    $(date)
+                </div>
+                <div>
+                    $(data)
+                </div>
+            </div>
+        </body>
+    </html>`;
+}
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.sendFile(path.join(__dirname, 'ui', createTemplate(articleOne)));
 });
 
 app.get('/article-two',function(req, res){
