56d86710d165d2deae93cae7297327a648e5ecc5
Pragya <pragyachik@gmail.com>
2016-10-29 14:22:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ function createTemplate(data){
             <meta name="viewport" content="width-device-width, initial-scale=1"/>
             <link href="/ui/style.css" rel="stylesheet" />
         </head>
-        <body bgcolor="#E6E6FA">
+        <body bgcolor=oldlace>
             <div class="container">
                 <div>
                     <a href='/'>Home</a>
