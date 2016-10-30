469c021598552d4cce267e2860d7a8333282b552
Pragya <pragyachik@gmail.com>
2016-10-30 19:04:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,14 @@ element.innerHTML="This is a Secret Webpage. Feel honoured to have found it";
 
 //Make the Dinasour move
 var image=document.getElementById("image");
-image.onclick = function(){
-    var interval='10px';
-    image.style.marginRight='100px';
+
+var marginLeft = 0;
+
+function moveRight(){
+    marginLeft = marginLeft+10;
+    image.style.marginLeft=marginLeft+"px";
 }
+
+image.onclick = function(){
+    var interval = setInterval(marginRight, 100);
+};
