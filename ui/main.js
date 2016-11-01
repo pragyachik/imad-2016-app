01a85c020ab26efd8a27ce95683f4858df32aaf9
Pragya <pragyachik@gmail.com>
2016-11-01 17:59:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -48,17 +48,39 @@ button.onclick=function(){
 
 
 // Submit button
-var submitInput = document.getElementById('name');
-var Name = submitInput.value;
-var Button = document.getElementById('submit_btn');
+
 Button.onclick = function(){
-    var names = ['Me','You','Everyone'];
-    var list = '';
-    for(var i=0; i<names.length; i++){
-        list+='<li>'+names[i]+'</li>';
-    }
-    var ol=document.getElementById('Name List');
-    ol.innerHTML=list;
+    
+    
+    //request
+    var request=new XMLHttpRequest();
+    
+    //response
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status===200){
+                var names = request.responseText;
+                names = JSON.parse(names);
+                for(var i=0; i<names.length; i++){
+                    list+='<li>'+names[i]+'</li>';
+                }
+                var ol=document.getElementById('Name List');
+                ol.innerHTML=list;
+            }
+        }
+    };
+    
+    var submitInput = document.getElementById('name');
+    var Name = submitInput.value;
+    var Button = document.getElementById('submit_btn');
+    
+    request.open('GET', 'http://pragyachik.imad.hasura-app.io/submit-name?name='+Name, true);
+    request.send(null);
+    
+    
+    
+    
+    
 };
 
 
