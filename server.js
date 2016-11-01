c38da5cfbd492d43cbff81cd740ed008b7e05e98
Pragya <pragyachik@gmail.com>
2016-11-01 10:18:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.get('/:articleName',function(req, res){
 });
 
 var counter=0;
-app.get('/counter',function(req, res){
+app.get('/c',function(req, res){
     counter=counter+1;
     res.send(counter.toString());
 });
