fd859df038a90dea93ba8f3b8aaf31686b606cec
Pragya <pragyachik@gmail.com>
2016-11-01 16:49:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 
 // Change text
 var element = document.getElementById("main text");
-element.innerHTML="This is a Secret Webpage. Feel honoured to have found it";
+element.innerHTML="This is a Secret Website. Feel honoured to have found it";
 
 //Make the Dinasour move
 var image=document.getElementById("image");
