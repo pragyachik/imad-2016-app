dc94f4a56cefd8e5865170f9a1e57bd0e1a19c8d
Pragya <pragyachik@gmail.com>
2016-11-01 11:03:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,9 +26,22 @@ function move(){
 //P6
 var button=document.getElementById('counter');
 
-var counter=0;
+
 button.onclick=function(){
-    counter++;
-    var span=document.getElementById('count');
-    span.innerHTML=counter;
-}
+    //request
+    var request=new XMLHttpRequest();
+    
+    //response
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status===200){
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter;
+            }
+        }
+    };
+    
+    request.open('GET', 'http://pragyachik.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
